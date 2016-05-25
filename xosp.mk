e809c96c0fd9c84c04cd9871d015f78e0d9ab254
Shreesha Murthy <shreeshu10@gmail.com>
2016-05-25 12:44:14 +0530

    Update xosp.mk

--- xosp.mk
+++ xosp.mk
@@ -2,7 +2,9 @@ $(call inherit-product, device/google/sprout4/sprout4.mk)
 
 # Common XOSP stuff
 $(call inherit-product, vendor/xosp/config/common_full_phone.mk)
-$(call inherit-product, vendor/xosp/config/xosp.mk
+
+# Call necessary apps
+$(call inherit-product, vendor/xosp/config/xosp.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
 
@@ -14,5 +16,5 @@ PRODUCT_MODEL := A1
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="sprout4"
-
+#BootAnimation
 XOSP_BOOTANIMATION_NAME := 480
