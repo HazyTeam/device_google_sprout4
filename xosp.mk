20c896a48fb6c0fb8f345e3f3c29fc0ff2ba7a2f
Shreesha <shreeshu10@gmail.com>
2016-05-25 12:39:59 +0530

    XOSP-IFY

--- xosp.mk
+++ xosp.mk
@@ -1,11 +1,12 @@
 $(call inherit-product, device/google/sprout4/sprout4.mk)
 
-# Common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Common XOSP stuff
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/config/xosp.mk
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
 
-PRODUCT_NAME := cm_sprout4
+PRODUCT_NAME := xosp_sprout4
 PRODUCT_DEVICE :=sprout4
 PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
@@ -13,3 +14,5 @@ PRODUCT_MODEL := A1
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="sprout4"
+
+XOSP_BOOTANIMATION_NAME := 480
